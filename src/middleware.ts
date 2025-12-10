5be4c8cd4fa739d5313815a736863d738dcb3a35
Shadowcats Admin <admin@shadowcats.com>
2025-12-10 15:00:19 -0600

    Fix versions and disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,6 @@ import { authConfig } from "./auth.config"
 export default NextAuth(authConfig).auth
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)"],
+    // matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)"],
+    matcher: [], // Disable middleware for now
 }
